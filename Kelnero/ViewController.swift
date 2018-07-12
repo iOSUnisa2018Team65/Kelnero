8a474b3cf4cb53ca58a4a3e3c03b71af8e9776c9
Giulia Sellitto <giuliasellitto.gs@gmail.com>
2018-07-12 21:17:36 +0200

    Initial project upload

---
+++ Kelnero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kelnero
+//
+//  Created by Sellitto Giulia on 12/07/2018.
+//  Copyright © 2018 Team 6_5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
