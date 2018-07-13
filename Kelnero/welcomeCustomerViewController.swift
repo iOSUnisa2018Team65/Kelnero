53fde6e7c8aa770434f24137a3dc42c11ec03ef4
Nello Panariello <killetore@gmail.com>
2018-07-13 15:19:51 +0200

    Inserito scan QRCode

---
+++ Kelnero/welcomeCustomerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  welcomeCustomerViewController.swift
+//  Kelnero
+//
+//  Created by Panariello Aniello on 13/07/18.
+//  Copyright © 2018 Team 6_5. All rights reserved.
+//
+
+import UIKit
+
+class welcomeCustomerViewController: UIViewController {
+    var qrCode: String = ""
+    @IBOutlet var myLabel: UILabel!
+    override func viewWillAppear(_ animated: Bool) {
+        myLabel.text = qrCode
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
