c950b69893c5a404aed1610817c53dda903b0b08
Giulia Sellitto <giuliasellitto.gs@gmail.com>
2018-07-18 17:40:06 +0200

    Gestione degli ordini nel database

--- Kelnero/OrderRowModel.swift
+++ Kelnero/OrderRowModel.swift
@@ -7,7 +7,93 @@
 //
 
 import UIKit
+import CloudKit
 
 class OrderRowModel: NSObject {
+    
+    // table and fields names
+    static let recordType = "OrderRow"
+    static let restaurantIdField = "restaurant_Id"
+    static let tableField = "table"
+    static let dishField = "dish_Name"
+    static let quantityField = "quantity"
+    static let stateField = "state"
+    
+    
+    class func addNew(orderRow toAdd: OrderRow, completionHandler handler: @escaping (OrderRow, Error?) -> Void) {
+        print("Saving order row")
+        let key = toAdd.restaurant.id + "_" + String(toAdd.table) + "_" + toAdd.dish.name
+        let recordId = CKRecordID(recordName: key)
+        let record = CKRecord(recordType: recordType, recordID: recordId)
+        
+        record[restaurantIdField] = toAdd.restaurant.id as NSString
+        record[tableField] = toAdd.table as NSNumber
+        record[dishField] = toAdd.dish.name as NSString
+        record[quantityField] = toAdd.quantity as NSNumber
+        record[stateField] = toAdd.state as NSNumber
+        
+        // saving
+        let container = CKContainer.default()
+        let db = container.publicCloudDatabase
+        
+        db.save(record) {
+            (savedRecord, error) in
+            handler(toAdd, error)
+        }
+    }
+    
+    
+    class func getAllOrderRowsByTable(restaurantId restId: String, tableNumber t: Int, completionHandler handler: @escaping ([OrderRow], Error?) -> (Void)) {
+        var orders = [OrderRow]()
+        var predicate1:NSPredicate = NSPredicate(format: "%K == %@", restaurantIdField, restId)
+        var predicate2:NSPredicate = NSPredicate(format: "%K == %@", tableField, t as NSNumber)
+        var whereClause:NSPredicate  = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1,predicate2] )
+        //var whereClause = NSPredicate(format: "%K == %@ AND %K == %@", restaurantIdField, restId, tableField, t)
+        var query = CKQuery(recordType: recordType, predicate: whereClause)
+        var rest: Restaurant? = nil
+        RestaurantModel.getById(idToSearch: restId) {
+            (r, error) in
+            if let e = error {
+                handler(orders, e)
+                return
+            }
+            else {
+                rest = r!
+                
+                let container = CKContainer.default()
+                let db = container.publicCloudDatabase
+                
+                db.perform(query, inZoneWith: nil) {
+                    (records, error) in
+                    if let e = error {
+                        handler(orders, e)
+                    }
+                    else {
+                        for fetchedRecord in records! {
+                            
+                            var table = fetchedRecord[tableField] as! Int
+                            var quantity = fetchedRecord[quantityField] as! Int
+                            var state = fetchedRecord[stateField] as! Int
+                            var dishName = fetchedRecord[dishField] as! String
+                            DishModel.getDishByName(restaurantId: restId, dishName: dishName) {
+                                (d, error) in
+                                if let e = error {
+                                    print(e)
+                                }
+                                else {
+                                    var order = OrderRow(restaurant: rest!, table: table, dish: d!, quantity: quantity, state: state)
+                                    orders.append(order)
+                                    print("ok")
+                                }
+                            }
+                        }
+                       handler(orders, nil)
+                    }
+                    
+                }
+            }
+        }
+    }
 
+    
 }
