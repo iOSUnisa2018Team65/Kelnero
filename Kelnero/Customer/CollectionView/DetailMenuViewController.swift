408d5362cefad64f4ed799b4d349527653023388
Nello Panariello <killetore@gmail.com>
2018-07-19 12:13:49 +0200

    update

--- Kelnero/Customer/CollectionView/DetailMenuViewController.swift
+++ Kelnero/Customer/CollectionView/DetailMenuViewController.swift
@@ -12,6 +12,7 @@ class DetailMenuViewController: UIViewController {
     
     var dishDetail: Dish?
     
+    @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var imageDish: UIImageView!
     
@@ -19,13 +20,20 @@ class DetailMenuViewController: UIViewController {
     @IBAction func stepperQuantity(_ sender: UIStepper) {
         quantityLabel.text = Int(sender.value).description
     }
-
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     @IBAction func addToOrderButton(_ sender: Any) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.maximumValue = 10
+        stepper.minimumValue = 1
         imageDish.image = dishDetail?.photo
+        nameLabel.text = dishDetail?.name
+        descriptionLabel.text = dishDetail?.descr
 
         // Do any additional setup after loading the view.
     }
