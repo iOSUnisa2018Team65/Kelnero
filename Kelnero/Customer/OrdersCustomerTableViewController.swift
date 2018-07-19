c28b052c08497ba143ca2aea14e9739e5d15bac8
Amorosi Sara <amorosisara@sal-04.local>
2018-07-19 11:29:50 +0200

    l

--- Kelnero/Customer/OrdersCustomerTableViewController.swift
+++ Kelnero/Customer/OrdersCustomerTableViewController.swift
@@ -41,7 +41,7 @@ class OrdersCustomerTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 
+        return orders.count
     }
     
     
